a611c8f0ae3b9a97058f826561b7391f0896e609
Antonio Bri Pérez <antoniobriperez@icloud.com>
2024-01-20 16:11:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
-});
-
 const port = process.env.PORT || 3000;
 const dbURL =  'mongodb://user:123@18.101.118.156:27017/db';
-server.listen(port, () => {
-  console.log(`Node.js server is running on port ${port} with DB connection URL ${dbURL}`);
-});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`Node.js server is running on port ${port} with DB connection URL ${dbURL}`);
+}).listen(port);
